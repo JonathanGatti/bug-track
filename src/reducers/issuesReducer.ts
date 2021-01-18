9c0849da89e18dca072a78093751bb3b4472921e
jonathan gatti <jonathangatti2@gmail.com>
2021-01-18 08:43:44 +0000

    issues reducers

--- src/reducers/issuesReducer.ts
+++ src/reducers/issuesReducer.ts
@@ -1,4 +1,5 @@
-import {FETCH_ISSUE, FETCH_ISSUES, CREATE_ISSUE, DELETE_ISSUE}from '../actions/types';
+import {FETCH_ISSUE, FETCH_ISSUES, CREATE_ISSUE, DELETE_ISSUE, EDIT_ISSUE}from '../actions/types';
+import _ from 'lodash';
 import {Issue} from '../interfaces';
 
 interface Action {
@@ -6,12 +7,18 @@ interface Action {
   payload: Issue[]
 }
 
-export const issuesReducer = (state = [], action: any) => {
+export const issuesReducer = (state = {}, action: any) => {
   switch(action.type){
     case FETCH_ISSUES:
-      return {...state, ...action.payload}
+      return {...state, ..._.mapKeys(action.payload, '_id')}
     case FETCH_ISSUE:
       return {...state, [action.payload._id]: action.payload}
+    case CREATE_ISSUE:
+      return {...state, [action.payload._id]: action.payload}
+    case EDIT_ISSUE:
+      return {...state, [action.payload._id]: action.payload}
+    case DELETE_ISSUE:
+      return _.omit(state, action.payload)
     default:
       return state;
   }
